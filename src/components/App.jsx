a6013d47cf9d17b21ee58afe6c65fcd216002699
Kateryna Tolstykh <agrratt@gmail.com>
2022-05-04 13:48:58 +0200

    deploy-4

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,12 +7,10 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        textTransform: 'uppercase',
-        color: '#010101',
+        color: '#010101'
       }}
     >
       React homework template
     </div>
   );
-  
 };
